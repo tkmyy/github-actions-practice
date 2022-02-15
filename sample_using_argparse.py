7721167714265e2214f7acf43e892175fd272890
takumi <tkmyy0201@gmail.com>
2022-02-15 14:30:27 +0900

    add script using argparse

---
+++ sample_using_argparse.py
@@ -0,0 +1,26 @@
+import argparse
+
+
+def main(num_1: int, num_2: int) -> None:
+    res = calc_sum(num_1, num_2)
+    print(res)
+
+
+def calc_sum(a: int, b: int) -> int:
+    return a + b
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--num_1",
+        type=int
+    )
+    parser.add_argument(
+        "--num_2",
+        type=int
+    )
+    args = parser.parse_args()
+    num_1 = args.num_1
+    num_2 = args.num_2
+    main(num_1=num_1, num_2=num_2)
